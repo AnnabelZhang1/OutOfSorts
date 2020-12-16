3c10eda2acc28d884f37ebc249d76bcdd2302e29
AnnabeZhang1 <azhang22@stuy.edu>
2020-12-16 12:33:52 -0500

    added random tester

--- SortsTester.java
+++ SortsTester.java
@@ -1,10 +1,21 @@
+import java.util.Random;
 public class SortsTester{
 
   public static void main(String[] args){
     int[] a = new int[]{1,3,6,4,7};
     Sorts.bubbleSort(a);
-    for (int i = 0; i < a.length; i++){
+    for (int i = 0; i < a.length; i++)
       System.out.print(a[i] + ",");
+
+    System.out.println("");
+
+    int[] b = new int[]{0,0,0,0,0,0,0};
+    Random rng = new Random();
+    for(int i = 0; i < 7; i++)
+      b[i] = (rng.nextInt() % 1000);
+    Sorts.bubbleSort(b);
+    for (int i = 0; i < a.length; i++){
+      System.out.print(b[i] + ",");
     }
   }
 }
